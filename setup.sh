05051fbea7354cf5dce073327fd3114d567caa64
sufiahlmn <aisyahhlmn09@gmail.com>
2024-12-30 03:16:26 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~.streamlit
+
+echo 
+[general]n
+email = your-email@domain.comn
+  ~.streamlitcredentials.toml
+
+echo 
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+  ~.streamlitconfig.toml
